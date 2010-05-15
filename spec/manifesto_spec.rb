367b5bb3734e620c5de4fbe387b53dad1efcbfca
John Topley <john@johntopley.com>
2010-05-15 19:54:41 +0100

    Added outline examples

--- spec/manifesto_spec.rb
+++ spec/manifesto_spec.rb
@@ -1,6 +1,27 @@
 require File.dirname(__FILE__) + '/spec_helper'
 
 describe Manifesto do
-  context "" do
+  it "should validate that its directory argument is a real directory" do
+  end
+  
+  it "should validate that its compute_hash argument is a boolean" do
+  end
+  
+  it "should return a list of files within the default directory" do
+  end
+  
+  it "should return a list of files within the specified directory" do
+  end
+  
+  it "should compute a hash of the files within the default directory" do
+  end
+  
+  it "should compute a hash of the files within the specified directory" do
+  end
+  
+  it "shouldn't include hidden files or directories within the default directory" do
+  end
+  
+  it "shouldn't include hidden files or directories within the specified directory" do
   end
 end
