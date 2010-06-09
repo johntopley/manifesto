1c4cf9d5713a3d8ec835cc246aa781120e8de394
John Topley <john@johntopley.com>
2010-06-09 11:01:01 +0100

    Formatting to taste

--- spec/manifesto_spec.rb
+++ spec/manifesto_spec.rb
@@ -1,9 +1,7 @@
 require File.dirname(__FILE__) + '/spec_helper'
 
 describe Manifesto do
-
   describe ".validate_arguments" do
-
     it "should raise ArgumentError if directory is not a real directory" do
       expect{ Manifesto.validate_arguments('', false) }.to raise_error(ArgumentError)
     end
@@ -11,14 +9,9 @@ describe Manifesto do
     it "should raise ArgumentError if compute_hash is not a boolean" do
       expect{ Manifesto.validate_arguments('.', nil) }.to raise_error(ArgumentError)
     end
-
   end
 
-  # You dont need to test the compute hash for default as well as the specified directory, test for either should
-  # suffice, similarly testing for hidden files or directories.
-
   describe ".cache" do
-
     before(:each) do
       Manifesto.stub!(:validate_arguments).and_return(true)
     end
@@ -29,25 +22,20 @@ describe Manifesto do
     end
 
     context "when default directory" do
-
       it "should get file paths from the default directory" do
         Manifesto.should_receive(:get_file_paths).with('./public').and_return([])
         Manifesto.cache
       end
-
     end
 
     context "when directory is specified" do
-
       it "should get file path from within the specified directory" do
         Manifesto.should_receive(:get_file_paths).with('./mobile').and_return([])
         Manifesto.cache(:directory => './mobile')
       end
-
     end
 
     context "when there are no files in the directory" do
-
       before(:each) do
         Manifesto.stub!(:get_file_paths).and_return([])
       end
@@ -56,18 +44,15 @@ describe Manifesto do
         Manifesto.should_receive(:compute_file_contents_hash).never
         Manifesto.cache
       end
-
     end
 
     context "when there are files in the directories" do
-
       it "should check the file" do
         File.should_receive(:file?)
         Manifesto.cache
       end
 
       context "and path is for the directory or symlink" do
-
         before(:each) do
           Manifesto.stub!(:get_file_paths).and_return(['public/dir1', 'public/symlink'])
           File.stub!(:file?).and_return(false)
@@ -82,11 +67,9 @@ describe Manifesto do
           Manifesto.should_receive(:normalize_path).never
           Manifesto.cache
         end
-
       end
 
       context "and path is for the hidden file" do
-
         before(:each) do
           Manifesto.stub!(:get_file_paths).and_return(['public/.hiddenfile'])
           File.stub!(:file?).and_return(true)
@@ -101,11 +84,9 @@ describe Manifesto do
           Manifesto.should_receive(:normalize_path).never
           Manifesto.cache
         end
-
       end
 
       context "and path is for a valid file" do
-
         before(:each) do
           Manifesto.stub!(:get_file_paths).and_return(['/public/file1'])
           File.stub!(:file?).and_return(true)
@@ -123,7 +104,6 @@ describe Manifesto do
         end
 
         context "and return an array of values" do
-
           it "should return an array of 4 elements" do
              Manifesto.cache.size.should eql(4)
           end
@@ -143,13 +123,8 @@ describe Manifesto do
           it "should return the hash" do
             Manifesto.cache.should be_include("# Hash: f2d6ac219e22a0eed707a24d65777a0e\n")
           end
-
         end
-
       end
-
     end
-
   end
-
 end
